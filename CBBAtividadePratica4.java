9b81f64224036eeff8ae38ec9ce77bf5ead8b5b9
Alana Cristina <86081104+llanacristina@users.noreply.github.com>
2022-10-25 19:32:06 -0300

    Enviando atividades do Livro CBB

---
+++ CBBAtividadePratica4.java
@@ -0,0 +1,14 @@
+
+/**
+ * Atividade do Livro Chagas, Baruque & Baruque (2010): questão 4 página 84.
+ *
+ */
+public class CBBAtividadePratica4
+{
+    public static void main(String[] args){
+        int num;
+        for(num = 0;num <= 100;num++){
+            System.out.println(num);
+        }
+    }
+}
